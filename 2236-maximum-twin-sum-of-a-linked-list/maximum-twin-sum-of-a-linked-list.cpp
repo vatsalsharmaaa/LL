382b2d8831c63cec9387187a4a4171e51495789b
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-02-06 11:45:37 +0530

    Time: 3 ms (67.11%) | Memory: 124.4 MB (67.50%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -1,21 +1,57 @@
 //APPROACH 1 using vectors
 
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         vector<int>curr;
+//         ListNode * temp=head;
+//         while(temp!=NULL){
+//             curr.push_back(temp->val);
+//             temp=temp->next;
+//         }   
+//         int n=curr.size();
+//         int maxi=INT_MIN;
+//         int i=0,j=n-1;
+//         while(i<j){
+//             maxi= max(maxi,curr[i]+curr[j]);
+//             i++;j--;
+//         }
+//         return maxi;
+//     }
+// };
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        vector<int>curr;
-        ListNode * temp=head;
-        while(temp!=NULL){
-            curr.push_back(temp->val);
-            temp=temp->next;
-        }   
-        int n=curr.size();
-        int maxi=INT_MIN;
-        int i=0,j=n-1;
-        while(i<j){
-            maxi= max(maxi,curr[i]+curr[j]);
-            i++;j--;
+       
+       ListNode *fast=head;
+       ListNode* slow = head;
+       while(fast!=NULL &&fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+
+       }
+       ListNode * mid=slow;
+
+       //2. reverese second half of ll
+        ListNode *prev=NULL;
+        ListNode *nextNode=NULL;
+        while(mid!=NULL){
+            nextNode = mid->next;
+            mid->next=prev;
+            prev=mid;
+            mid=nextNode;
         }
-        return maxi;
+        int result = 0;
+        ListNode * curr=head;
+
+        while(prev!=NULL){
+            result=max(result,curr->val + prev->val);
+            curr=curr->next;
+            prev=prev->next;
+        }
+        return result;
+
+
+
     }
 };
